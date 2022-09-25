aa791c6c5325f1ff2f2af756feceeece25002dd2
nitneuq <quentin@miladz.eu>
2022-09-25 17:58:55 +0200

    Amélioration filtration fichier

--- TriePhoto.py
+++ TriePhoto.py
@@ -3,36 +3,45 @@
 # --- Si aucune date n'est trouvé on fait un trie: Sans Exif, Pas de date ou Illisible
 
 
+from calendar import c
+from PIL.ExifTags import TAGS
 import os
-from exif import Image
-
-chemin = input('Repertoire photo à Trier:')
-no_lisible = '0 - Incconnue'
-no_date = '1 - Pas de Date'
-no_exif = '2 - Sans Exif'
 
 
-def ImageLisible(racine):
-    # Controle si l'image n'est pas corompue
-    import cv2  # Lib OpenCV
+def ImageValide(racine):
+    import imghdr
+    import os
 
-    if os.path.isfile(racine):
-        image = cv2.imread(racine)  # Ouverture de l'image avec OpenCV
-        return image is not None
-    else:
-        return False  # Image n'est pas un fichier
+    _, ext = os.path.splitext(racine)
+    try:
+        t_fichier = imghdr.what(racine)
+
+        f_valide = ['png', 'jpg', 'jpeg', 'tiff', 'bmp', 'gif']
+
+        if ext[1:].lower() in f_valide and t_fichier in f_valide:
+            return t_fichier
+    except OSError:
+        print('erreur')
 
 
 def ExifDatetime(c_fichier):
-    from exif import Image
-    with open(c_fichier, 'rb') as image_file:
-        my_image = Image(image_file)
-        for attribu in chemin(my_image):
-            return attribu == 'datetime'
-    image_file.close()
+
+    from PIL import Image
+
+    try:
+        img = Image.open(c_fichier)
+        img_exif = img.getexif()
+
+        for tagid in img_exif:
+            tagname = TAGS.get(tagid, tagid)
+            value = img_exif.get(tagid)
+            if tagname == 'DateTime':
+                return value
+    except OSError:
+        print('erreur')
 
 
-def MoveImage(source, destination):
+def MoveFile(source, destination):
     import os
     import shutil
 
@@ -49,16 +58,42 @@ def MoveImage(source, destination):
             print('Oups !!')
 
 
+chemin = input('Repertoire photo à Trier:')
+no_lisible = '0 - Incconnue'
+no_date = '1 - Pas de Date'
+no_exif = '2 - Sans Exif'
+f_invalide = []
+f_valide = []
+
+checker = open('manifest.txt', 'w')
+
 for dossier in list(os.walk(chemin)):
+
+    c_dossier = os.path.join(chemin, dossier[0])
+    print(c_dossier)
 
     for fichier in dossier[2]:
 
-        c_dossier = os.path.join(chemin, dossier[0])
         c_fichier = os.path.join(c_dossier, str(fichier))
 
         if os.path.isfile(c_fichier):
 
-            image_file = open(c_fichier, 'rb')
+            if ImageValide(c_fichier):
+                date = ExifDatetime(c_fichier)
+                if date is not None:
+                    print(f"Fichier valide {c_fichier} - {date}")
+                    json = {'move': False, 'date': date, 'path': c_fichier}
+
+                    checker.write(f"{json}\n")
+
+            else:
+                f_invalide.append(c_fichier)
+        else:
+            print('Pas un fichier')
+
+
+"""
+image_file = open(c_fichier, 'rb')
             # if ImageLisible(c_fichier):
             try:
                 print(c_fichier)
@@ -81,5 +116,5 @@ for dossier in list(os.walk(chemin)):
                 image_file.close()
                 print('Illisible')
                 MoveImage(c_fichier, os.path.join(chemin, no_lisible))
-        else:
-            print('Pas un fichier')
+
+"""
